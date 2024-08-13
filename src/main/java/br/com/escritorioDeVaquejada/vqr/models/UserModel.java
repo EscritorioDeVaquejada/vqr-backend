c38a2440847722f4fe466870c6660b383a417a1b
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-08-13 15:03:15 -0300

    feat: making UserModel class implement UserDetails

--- src/main/java/br/com/escritorioDeVaquejada/vqr/models/UserModel.java
+++ src/main/java/br/com/escritorioDeVaquejada/vqr/models/UserModel.java
@@ -1,14 +1,17 @@
 package br.com.escritorioDeVaquejada.vqr.models;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 @Entity
 @Table(name = "users")
-public class UserModel implements Serializable {
+public class UserModel implements Serializable, UserDetails {
     @Serial
     private static final long serialVersionUID=1L;
     @Id
@@ -16,9 +19,44 @@ public class UserModel implements Serializable {
     @Column(name = "user_id")
     private UUID id;
     @Column(unique = true)
-    private String email;
+    private String username;
     private String password;
+    private String role;
     @OneToMany(mappedBy = "user")
     private List<TicketModel> ticketList;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
